c669f01b6e1f6618c5d195dae01647b3f1a17201
Gabriel <marcosgabrielgx@gmail.com>
2022-10-24 17:07:02 -0300

    First Deploy

---
+++ SistemaDeTarefas/Enums/StatusTarefa.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A fazer")]
+        Afazer = 1,
+        EmAndamento = 2,
+        Concluido = 3,
+    }
+}
